54986131bfec575e9c366553d3690d11ebe57589
Soledad Leiva <soledad.leiva98@gmail.com>
2022-08-09 03:21:43 -0300

    punto de pago

--- src/main/java/com/utn/frba/dds/model/metodosDePago/CreadorPuntoDePago.java
+++ src/main/java/com/utn/frba/dds/model/metodosDePago/CreadorPuntoDePago.java
@@ -3,5 +3,18 @@ package com.utn.frba.dds.model.metodosDePago;
 
 
 public interface CreadorPuntoDePago {
-    
+    public static PuntoDePago getPuntoDePago(String puntoDePago) {
+
+        if (puntoDePago == null) {
+            return null;
+        }
+
+        if (puntoDePago.equalsIgnoreCase("PAGAYA")) {
+            return new PagaYa();
+        else {
+            throw new IllegalStateException("Unexpected value: " + puntoDePago);
+        }
+    	}
+    }
 }
+
